af38c6adbcad08e03101985d27ce0f6f17084374
Pratchayanon Thainngoen <feenfrink@gmail.com>
2025-05-17 16:39:55 +0700

    feat: add about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HomeSection from "@/components/Sections/Home";
+import AboutSection from "@/components/Sections/About";
 
 export default function Home() {
   return (
     <>
       <HomeSection />
+      <AboutSection />
     </>
   );
 }
---
+++ components/Sections/About.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Image from "next/image";
+
+const About = () => {
+  return (
+    <div className="bg-linear-to-t from-black to-[#110622] bg-no-repeat bg-cover bg-center flex flex-col lg:flex-row items-center justify-center py-20 relative px-5">
+      <div className="absolute bg-[url(/assets/images/AboutBG.png)] inset-0 bg-cover bg-center"></div>
+      <Image
+        src="/assets/images/logo.png"
+        alt="logo"
+        width="800"
+        height="650"
+        className="z-10"
+      />
+      <div className="text-white space-y-5 z-10">
+        <h1 className="space-x-2 text-4xl lg:text-5xl">
+          <span className="text-[#FFDCC9] [font-family:var(--font-anta)]">
+            BOOTCAMP
+          </span>
+          <span className="font-medium">คืออะไร?</span>
+        </h1>
+        <p className="font-medium text-lg lg:text-2xl max-w-[700px]">
+          เป็นกิจกรรมที่จัดขึ้นเพื่อเสริมสร้างและพัฒนาทักษะทางด้านเทคโนโลยีให้แก่นักเรียนระดับชั้นมัธยมศึกษา
+          พร้อมทั้งเปิดโอกาส
+          ให้นักเรียนได้มีความรู้ความเข้าใจในพื้นฐานทางด้านการเขียนโปรแกรมมากขึ้นรวมถึงแนะแนวทางในการศึกษาต่อทางด้านเทคโนโลยีภายในอนาคตให้แก่นักเรียน
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
