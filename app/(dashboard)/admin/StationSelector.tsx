3aecac7d2c92e58f347fb1388beb1ddce0c9e5cf
Jozho <sippakorn1337@gmail.com>
2025-05-18 19:17:39 +0700

    refactor(StationSelector): update mock data structure and improve layout

--- app/(dashboard)/admin/StationSelector.tsx
+++ app/(dashboard)/admin/StationSelector.tsx
@@ -1,33 +1,31 @@
 "use client";
 
-import useUserStore from "@/hooks/userStore";
 import { Card, CardBody, CardFooter } from "@heroui/card";
-import { Image } from "@heroui/react";
-import { useRouter } from "next/navigation";
+import { Button, Image, Link } from "@heroui/react";
 
 // TODO: move type to types folder
 type Station = {
   id: number;
   name: string;
+  image: string;
 };
 
 // Mock data
 const mockStations = [
-  { id: 1, name: "สถานี A" },
-  { id: 2, name: "สถานี B" },
-  { id: 3, name: "สถานี C" },
-  { id: 4, name: "สถานี D" },
-  { id: 5, name: "สถานี E" },
-  { id: 6, name: "สถานี F" },
+  { id: 1, name: "สถานี A", image: "/assets/images/teams/TeamBlue.png" },
+  { id: 2, name: "สถานี B", image: "/assets/images/teams/TeamGreen.png" },
+  { id: 3, name: "สถานี C", image: "/assets/images/teams/TeamOrange.png" },
+  { id: 4, name: "สถานี D", image: "/assets/images/teams/TeamPink.png" },
+  { id: 5, name: "สถานี E", image: "/assets/images/teams/TeamPurple.png" },
+  { id: 6, name: "สถานี F", image: "/assets/images/teams/TeamYellow.png" },
 ] as Station[];
 
 export default function StationSelector() {
-  const { user } = useUserStore();
-
   return (
-    <section className="w-full">
-      <h1 className="text-center text-3xl">เลือกสถานีที่คุณประจำอยู่</h1>
-      <p>{user?.username}</p>
+    <section className="w-full space-y-10">
+      <h1 className="text-center text-3xl text-white [font-family:var(--font-anta)]">
+        Stamp Station
+      </h1>
       <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
         {mockStations.map((station) => (
           <StationSelectorItem key={station.id} data={station} />
@@ -38,33 +36,29 @@ export default function StationSelector() {
 }
 
 const StationSelectorItem = ({ data }: { data: Station }) => {
-  const { push } = useRouter();
-
-  const handleClick = () => {
-    push(`/admin/station/${data.id}`);
-  };
-
   return (
     <Card
-      isFooterBlurred
-      isPressable
-      className="border-none"
+      isBlurred
+      className="border-none bg-white/0 max-w-[180px] lg:max-w-[200px]"
       radius="lg"
-      onPress={handleClick}
+      shadow="sm"
     >
       <CardBody>
         <Image
           alt={data.name}
-          className="object-cover"
-          height={200}
-          src="https://github.com/ku-t3ch.png"
-          width={200}
+          className="object-contain w-full"
+          src={data.image}
         />
+        <h2 className="text-white text-center">{data.name}</h2>
       </CardBody>
-      <CardFooter className="justify-center before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-        <p className="text-sm light:text-white/80 dark:text-zinc-800">
-          {data.name}
-        </p>
+      <CardFooter className="justify-center overflow-hidden py-1 bottom-1 w-[calc(100%_-_8px)] ml-1 mb-2 z-10">
+        <Button
+          as={Link}
+          href={`/admin/station/${data.id}`}
+          className="bg-[#F182D2] hover:bg-[#F182D2]/90 text-white"
+        >
+          ดูรายละเอียด
+        </Button>
       </CardFooter>
     </Card>
   );
