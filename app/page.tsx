3f26331005a4985bd8e5838c7bf50c21013a26c3
Pratchayanon Thainngoen <feenfrink@gmail.com>
2025-05-18 22:03:50 +0700

    feat: add our camp section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import HomeSection from "@/components/Sections/Home";
 import AboutSection from "@/components/Sections/About";
 import TimelineSection from "@/components/Sections/Timeline";
+import OurCampSection from "@/components/Sections/OurCamp";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <HomeSection />
       <AboutSection />
       <TimelineSection />
+      <OurCampSection />
     </>
   );
 }
