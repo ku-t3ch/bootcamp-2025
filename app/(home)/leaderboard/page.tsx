39cf76064a0cf76666e32cf6504f54e255f6a516
Jozho <sippakorn1337@gmail.com>
2025-05-18 14:36:11 +0700

    remove(leaderboard.page): fragment

--- app/(home)/leaderboard/page.tsx
+++ app/(home)/leaderboard/page.tsx
@@ -1,14 +1,9 @@
-import React from 'react';
-
-import Leaderboard from '@/components/Leaderboard';
+import Leaderboard from "@/components/Leaderboard";
 
 export default function Page() {
-    console.log("Clear here");
-    return (
-        <>
-            <section className="min-h-screen bg-secondary-texture bg-center">
-                    <Leaderboard />
-            </section>
-        </>
-    )
+  return (
+    <section className="min-h-screen bg-secondary-texture bg-center">
+      <Leaderboard />
+    </section>
+  );
 }
