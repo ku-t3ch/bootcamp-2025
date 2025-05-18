b198337a81cf9d6e64a92dceacd45f87f9019a8b
Pratchayanon Thainngoen <feenfrink@gmail.com>
2025-05-18 15:56:58 +0700

    feat: add timeline section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HomeSection from "@/components/Sections/Home";
 import AboutSection from "@/components/Sections/About";
+import TimelineSection from "@/components/Sections/Timeline";
 
 export default function Home() {
   return (
     <>
       <HomeSection />
       <AboutSection />
+      <TimelineSection />
     </>
   );
 }
