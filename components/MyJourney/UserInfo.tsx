0b121a3b087127e9d49a1ceaee55842fc2de9978
Thanchida <thanchida.a@ku.th>
2025-05-22 02:01:50 +0700

    refactor: reuse user info if already loaded

--- components/MyJourney/UserInfo.tsx
+++ components/MyJourney/UserInfo.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { Image } from "@heroui/react";
 import { User } from "@/types/user";
-import axiosClient from "@/lib/axios";
+import useUserStore from "@/hooks/userStore";
 
 
 type UserInfo = User & {
@@ -12,37 +12,13 @@ type UserInfo = User & {
   teamScore: number;
 };
 
-const teamData = {
-  "a": { name: "Pink" },
-  "b": { name: "Orange" },
-  "c": { name: "Yellow" },
-  "d": { name: "Green" },
-  "e": { name: "Blue" },
-  "f": { name: "Purple" },
-};
-
 
 export default function UserInfo({setTeamId}: {setTeamId: React.Dispatch<React.SetStateAction<string | null>> }) {
-  const [user, setUser] = useState<UserInfo | null>(null);
+  const { user } = useUserStore();
 
   useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const res = await axiosClient.get('/auth/me');
-        if (res.data) {
-          const data = res.data["data"];
-          const teamKey = data.team as keyof typeof teamData;
-          data["logo_url"] = `/assets/images/teams/Team${teamData[teamKey].name}.png`;
-          setUser(data);
-          setTeamId(res.data["data"].team);
-        }
-      } catch (error) {
-        setUser(null);
-        console.error("Fetch user info failed:", error);
-      }
-    }
-    fetchUser();
-  }, [setTeamId]);
+    setTeamId(user?.team || null)
+  }, [setTeamId]);  
 
   if (!user) {
     return <p>Loading...</p>;
@@ -53,7 +29,7 @@ export default function UserInfo({setTeamId}: {setTeamId: React.Dispatch<React.S
       className="w-full bg-transparent border-2 rounded-[12px] px-8 py-4 flex items-center z-10 gap-4 border-[#9E99F2] shadow-[0_0_20px_10px_rgba(159,157,184,0.5)]"
     >
       <Image 
-        src={user.logo_url}   
+        src={user.logoUrl}   
         alt="Team Logo" 
         width="100"
         height="100" 
