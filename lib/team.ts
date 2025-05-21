6198ec07763752c218d2a07faf623918bc61e9ce
Jozho <sippakorn1337@gmail.com>
2025-05-22 01:28:21 +0700

    init: team color data

---
+++ lib/team.ts
@@ -0,0 +1,8 @@
+export const teamColorData = {
+  a: { name: "Pink" },
+  b: { name: "Orange" },
+  c: { name: "Yellow" },
+  d: { name: "Green" },
+  e: { name: "Blue" },
+  f: { name: "Purple" },
+};
