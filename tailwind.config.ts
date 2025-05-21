81fafeaf573b72e43d0b7ad6163d5bff280bb115
Jozho <sippakorn1337@gmail.com>
2025-05-21 19:55:59 +0700

    fix(tailwind): use typescript on configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,16 @@
-// tailwind.config.js
+import type { Config } from "tailwindcss";
 import { heroui } from "@heroui/react";
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'Fuchsia-blue': '#865EBF',
-        'Indigo': '#4541BF'
-      }
+        "Fuchsia-blue": "#865EBF",
+        Indigo: "#4541BF",
+      },
     },
   },
   darkMode: "class",
   plugins: [heroui()],
-};
+} satisfies Config;
