72d94db4ac92cd8d2a77f9d459e2555fb9cac069
chrisfoong <chris.owo@hotmail.com>
2025-05-23 20:47:35 +0700

    change BG to the same as footer

--- app/(landing)/leaderboard/page.tsx
+++ app/(landing)/leaderboard/page.tsx
@@ -1,10 +1,9 @@
 import LeaderboardDetail from "@/components/Leaderboard/LeaderboardDetail";
 import LeaderboardHeader from "@/components/Leaderboard/LeaderboardHeader";
 
-
 export default function Page() {
   return (
-    <section className="min h-screen bg-[url('/assets/images/HomeBG.gif')] bg-cover bg-center">
+    <section className="min h-screen bg-[url('/assets/images/TimelineBG.gif')] bg-cover bg-center">
       <div className="px-4 sm:px-6 md:px-12 lg:px-20 py-30 max-w-5xl mx-auto grid">
         <div className="text-4xl sm:text-3xl md:text-3xl font-bold mb-6 text-white" id="leader-box">
           <LeaderboardHeader />
