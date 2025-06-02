736a562c2d407801bc59ab74518a7c9b44e67e47
JozhoX <sippakorn1337@gmail.com>
2025-06-02 12:33:02 +0700

    feat: restructure admin page layout and add reviews button

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,13 +1,30 @@
-import StationSelector from "./StationSelector";
+"use client";
 
-export const dynamic = "force-dynamic";
+import { Button, Link } from "@heroui/react";
+import StationSelector from "./StationSelector";
+import { MessageCircle } from "lucide-react";
 
 export default function Page() {
   return (
-    <div className="min-h-screen flex justify-center items-center">
+    <div className="min-h-screen flex flex-col justify-center items-center">
       <div className="px-4 md:px-0 py-6 md:py-0">
         <StationSelector />
       </div>
+      <section className="space-y-2 pb-6">
+        <h1 className="text-center text-3xl text-white [font-family:var(--font-anta)]">
+          Administration
+        </h1>
+        <div className="flex justify-center">
+          <Button
+            as={Link}
+            href="/admin/reviews"
+            color="primary"
+            startContent={<MessageCircle size={20} />}
+          >
+            ความคิดเห็น
+          </Button>
+        </div>
+      </section>
     </div>
   );
 }
